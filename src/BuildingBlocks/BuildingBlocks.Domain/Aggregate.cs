821aeffac890b967818e3620aeae7a090c61ed9d
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-12-06 09:49:18 +0330

    Added BuildingBlocks.Core and Created entity and aggergate

---
+++ src/BuildingBlocks/BuildingBlocks.Domain/Aggregate.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Domain
+{
+    public class Aggregate<TKey> : Entity<TKey>
+    {
+
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Domain/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Domain
+{
+    public class Entity<TKey>
+    {
+        public TKey Id { get; protected set; }
+    }
+}
