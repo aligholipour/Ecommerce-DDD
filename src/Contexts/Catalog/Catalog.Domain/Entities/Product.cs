ff63f8d2b452ca9c09f8d988ffa78f76fd948c66
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-12-10 18:13:28 +0330

    Added Product entity

---
+++ src/Contexts/Catalog/Catalog.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Domain;
+
+namespace Catalog.Domain.Entities
+{
+    public class Product : AggregateRoot<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
