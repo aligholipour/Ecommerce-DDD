ff2874d1ef8f3b136225ef9507e149c3c06ddca9
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-12-11 17:56:03 +0330

    Added CatalogDbContext in Catalog bounded context

---
+++ src/Contexts/Catalog/Catalog.Infrastructure/Data/CatalogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Infrastructure.Data
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> option) : base(option)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
