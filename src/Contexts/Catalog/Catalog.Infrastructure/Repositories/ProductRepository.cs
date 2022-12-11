7f95d2e9aa4103b5d9ec46138e0793007295b6de
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-12-11 18:14:05 +0330

    Added Product repository in catalog bounded context

---
+++ src/Contexts/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Domain.Entities;
+using Catalog.Infrastructure.Data;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly CatalogDbContext _context;
+        public ProductRepository(CatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            await _context.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
