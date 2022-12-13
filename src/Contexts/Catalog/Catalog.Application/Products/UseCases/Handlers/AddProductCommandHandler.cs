2a1d54fc6e4508090e5d3cff607fd733ad47f209
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-12-13 11:29:50 +0330

    Added AddProductCommandHandler

---
+++ src/Contexts/Catalog/Catalog.Application/Products/UseCases/Handlers/AddProductCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Catalog.Application.Products.UseCases.Commands;
+using Catalog.Domain.Entities.Products;
+using Catalog.Infrastructure.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Products.UseCases.Handlers
+{
+    public class AddProductCommandHandler : IRequestHandler<AddProductCommand, int>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public AddProductCommandHandler(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(AddProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = _mapper.Map<Product>(request.AddProduct);
+
+            await _productRepository.AddProductAsync(product);
+
+            return product.Id;
+        }
+    }
+}
