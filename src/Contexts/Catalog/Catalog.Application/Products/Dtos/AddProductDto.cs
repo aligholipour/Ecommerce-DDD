c4e3573d5d4eb1ce147f72b6cefbf92a776e8d63
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-12-13 11:20:50 +0330

    Added AddProductCommand

---
+++ src/Contexts/Catalog/Catalog.Application/Products/Dtos/AddProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Application.Products.Dtos
+{
+    public class AddProductDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ src/Contexts/Catalog/Catalog.Application/Products/UseCases/Commands/AddProductCommand.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Application.Products.Dtos;
+using MediatR;
+
+namespace Catalog.Application.Products.UseCases.Commands
+{
+    public class AddProductCommand : IRequest<int>
+    {
+        public AddProductDto AddProduct { get; private set; }
+
+        public AddProductCommand(AddProductDto addProduct)
+        {
+            AddProduct = addProduct;
+        }
+    }
+}
