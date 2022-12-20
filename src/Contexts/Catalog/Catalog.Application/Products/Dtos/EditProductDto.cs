53529e29df411ef0e4c408b3551fe1f1ca22aae6
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-12-20 21:09:44 +0330

    Added EditProductCommand and EditProductDto

---
+++ src/Contexts/Catalog/Catalog.Application/Products/Dtos/EditProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.Application.Products.Dtos
+{
+    public class EditProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ src/Contexts/Catalog/Catalog.Application/Products/UseCases/Commands/EditProductCommand.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Application.Products.Dtos;
+
+namespace Catalog.Application.Products.UseCases.Commands
+{
+    public class EditProductCommand
+    {
+        public EditProductDto EditProductDto { get; private set; }
+
+        public EditProductCommand(EditProductDto editProductDto)
+        {
+            EditProductDto = editProductDto;
+        }
+    }
+}
