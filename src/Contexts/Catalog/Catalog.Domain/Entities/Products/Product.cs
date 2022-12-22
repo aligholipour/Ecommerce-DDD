9bd57d2f0396a5047f50c14f4f1601b7bb928049
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-12-22 18:26:56 +0330

    Added ChangePrice method in product entity for change price product

--- src/Contexts/Catalog/Catalog.Domain/Entities/Products/Product.cs
+++ src/Contexts/Catalog/Catalog.Domain/Entities/Products/Product.cs
@@ -1,4 +1,5 @@
 ﻿using BuildingBlocks.Domain;
+using Catalog.Domain.Entities.Exceptions;
 
 namespace Catalog.Domain.Entities.Products
 {
@@ -8,5 +9,13 @@ namespace Catalog.Domain.Entities.Products
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
+
+        public void ChangePrice(decimal price)
+        {
+            if (price <= 0)
+                throw new InvalidPriceValueException("The price is less than zero");
+
+            Price = price;
+        }
     }
 }
