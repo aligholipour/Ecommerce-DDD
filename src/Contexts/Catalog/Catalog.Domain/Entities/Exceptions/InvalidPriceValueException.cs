499199fc7a27e2ee5e609f6cc533e7f06cbde8c1
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-12-22 18:25:30 +0330

    Add InvalidPriceValueException in domain layer

---
+++ src/Contexts/Catalog/Catalog.Domain/Entities/Exceptions/InvalidPriceValueException.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Domain.Entities.Exceptions
+{
+    public class InvalidPriceValueException : Exception
+    {
+        public InvalidPriceValueException() { }
+
+        public InvalidPriceValueException(string message) : base(message) { }
+    }
+}
