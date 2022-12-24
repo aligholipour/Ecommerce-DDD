7bb82a0ba63f6c40383738a79b8e6c8878ca69d4
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-12-24 17:23:45 +0330

    Raising CheckoutShoppingCart Integration Event when the user checkout shopping cart

---
+++ src/Contexts/Basket/Basket.Application/ShoppingCarditems/Events/IntegrationEvent/CheckoutShoppingCartIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Core.Events;
+
+namespace Basket.Application.ShoppingCarditems.Events.IntegrationEvent
+{
+    public class CheckoutShoppingCartIntegrationEvent : IEvent
+    {
+        public Guid EventId => Guid.NewGuid();
+        public DateTime PublishDateTime => DateTime.Now;
+        public string UserId { get; set; }
+        public string ProductId { get; set; }
+    }
+}
