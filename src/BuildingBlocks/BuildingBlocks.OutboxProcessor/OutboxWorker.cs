6165a2a17e946a5483a80de214b1696ef17a127e
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-01-05 10:19:49 +0330

    Added OutboxWorker in OutboxProcessor

---
+++ src/BuildingBlocks/BuildingBlocks.OutboxProcessor/OutboxWorker.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace BuildingBlocks.OutboxProcessor
+{
+    public class OutboxWorker : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
