17a1eb881953098d1882db6065f650df3cd01e86
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-01-06 17:53:56 +0330

    Added IFilter in OutboxProcessor

---
+++ src/BuildingBlocks/BuildingBlocks.OutboxProcessor/Filtering/IFilter.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Core.Events;
+
+namespace BuildingBlocks.OutboxProcessor.Filtering
+{
+    public interface IFilter
+    {
+        void ShouldBePublish(IEvent @event);
+    }
+}
