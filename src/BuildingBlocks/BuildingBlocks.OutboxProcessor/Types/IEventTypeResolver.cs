edcae7955a171fffb685f30c8ffbfa0c1020d4fc
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-01-07 23:44:03 +0330

    Added IEventTypeResolver interface in OutboxProcessor

---
+++ src/BuildingBlocks/BuildingBlocks.OutboxProcessor/Types/IEventTypeResolver.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace BuildingBlocks.OutboxProcessor.Types
+{
+    public interface IEventTypeResolver
+    {
+        void AddTypesFromAssembly(Assembly assembly);
+        Type GetType(string typeName);
+    }
+}
