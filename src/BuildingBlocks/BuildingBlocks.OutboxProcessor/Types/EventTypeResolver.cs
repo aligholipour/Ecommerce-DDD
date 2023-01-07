c12810d7f895df771943d467cf6a31f142c1630f
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-01-08 00:10:38 +0330

    Implement EventTypeResolver in OutboxProcessor

---
+++ src/BuildingBlocks/BuildingBlocks.OutboxProcessor/Types/EventTypeResolver.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.Core.Events;
+using System.Reflection;
+
+namespace BuildingBlocks.OutboxProcessor.Types
+{
+    public class EventTypeResolver : IEventTypeResolver
+    {
+        private Dictionary<string, Type> _types = new Dictionary<string, Type>();
+
+        public void AddTypesFromAssembly(Assembly assembly)
+        {
+            var events = assembly.GetTypes().Where(type => typeof(IEvent).IsAssignableFrom(type)).ToList();
+
+            events.ForEach(a =>
+            {
+                _types.Add(a.Name, a);
+            });
+        }
+
+        public Type GetType(string typeName)
+        {
+            if (_types.ContainsKey(typeName))
+                return _types[typeName];
+
+            return null;
+        }
+    }
+}
