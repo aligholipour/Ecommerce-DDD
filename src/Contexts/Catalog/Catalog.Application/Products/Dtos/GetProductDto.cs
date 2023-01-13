2d67d1da3f13107e9f0a99965649be7c8c18d262
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-01-13 21:44:18 +0330

    Add GetProductDto in Catalog bounded context

---
+++ src/Contexts/Catalog/Catalog.Application/Products/Dtos/GetProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Application.Products.Dtos
+{
+    public class GetProductDto
+    {
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
