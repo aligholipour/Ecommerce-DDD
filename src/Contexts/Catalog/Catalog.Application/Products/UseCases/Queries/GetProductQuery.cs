a40ea01f776db5a587dce4a6163b5a0f30b43417
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-01-13 21:45:40 +0330

    Add GetProductQuery

---
+++ src/Contexts/Catalog/Catalog.Application/Products/UseCases/Queries/GetProductQuery.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Application.Products.Dtos;
+using MediatR;
+
+namespace Catalog.Application.Products.UseCases.Queries
+{
+    public class GetProductQuery : IRequest<GetProductDto>
+    {
+        public int ProductId { get; set; }
+    }
+}
