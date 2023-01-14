ebe057ad46a540cd7034c636b9a838f1586db7c6
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-01-14 20:14:05 +0330

    Add ProductConfigurations in Catalog bounded context

---
+++ src/Contexts/Catalog/Catalog.Infrastructure/Configurations/ProductConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catalog.Infrastructure.Configurations
+{
+    public class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(1000);
+        }
+    }
+}
