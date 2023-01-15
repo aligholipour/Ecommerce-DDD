23558eb4ecb03d80c5fb505beaa1bee16ad3fd9b
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-01-15 21:19:02 +0330

    Added ShoppingCardItemConfigurations in Basket bounded context

---
+++ src/Contexts/Basket/Basket.Infrastructure/Configurations/ShoppingCardItemConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using Basket.Domain.Entities.ShoppingCardItems;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Basket.Infrastructure.Configurations
+{
+    public class ShoppingCardItemConfigurations : IEntityTypeConfiguration<ShoppingCardItem>
+    {
+        public void Configure(EntityTypeBuilder<ShoppingCardItem> builder)
+        {
+            builder.Property(sh => sh.Amount).IsRequired();
+        }
+    }
+}
