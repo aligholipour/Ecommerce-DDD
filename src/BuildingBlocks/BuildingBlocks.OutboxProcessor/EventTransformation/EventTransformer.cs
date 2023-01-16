b6f359cb76add0d3c90830ed3be770cb3d4020b5
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-01-16 22:09:14 +0330

    Added EventTransformer implementation

---
+++ src/BuildingBlocks/BuildingBlocks.OutboxProcessor/EventTransformation/EventTransformer.cs
@@ -0,0 +1,13 @@
+﻿using BuildingBlocks.Core.Events;
+
+namespace BuildingBlocks.OutboxProcessor.EventTransformation
+{
+    public abstract class EventTransformer<T> : IEventTransformer where T : IEvent
+    {
+        protected abstract IEvent Transform(T @event);
+        public IEvent Transform(IEvent @event)
+        {
+            return Transform(@event);
+        }
+    }
+}
