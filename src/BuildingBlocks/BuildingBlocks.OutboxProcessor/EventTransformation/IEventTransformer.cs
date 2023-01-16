a56c52d30f8c7f9192446f08cd259988825b80fe
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-01-16 22:08:25 +0330

    Added IEventTransformer interface in OutboxProcessor

---
+++ src/BuildingBlocks/BuildingBlocks.OutboxProcessor/EventTransformation/IEventTransformer.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Core.Events;
+
+namespace BuildingBlocks.OutboxProcessor.EventTransformation
+{
+    public interface IEventTransformer
+    {
+        IEvent Transform(IEvent @event);
+    }
+}
