d164798488d590fdc3967e1be37391e7042ae765
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-02-02 18:28:11 +0330

    Add Invalid product quantity exception

---
+++ src/Contexts/Catalog/Catalog.Domain/Entities/Exceptions/InvalidProductQuantityException.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Domain.Entities.Exceptions
+{
+    public class InvalidProductQuantityException : Exception
+    {
+        public InvalidProductQuantityException() { }
+
+        public InvalidProductQuantityException(string message) : base(message) { }
+    }
+}
