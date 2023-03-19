8046af7dcdf400bdddae832d8dc2ed0a6314ce09
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-03-20 01:02:03 +0330

    Add AddCategoryDto

---
+++ src/Contexts/Catalog/Catalog.Application/Categories/Dtos/AddCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Application.Categories.Dtos
+{
+    public class AddCategoryDto
+    {
+        public string Name { get; set; }
+        public int ParentCategoryId { get; set; }
+        public bool Published { get; set; }
+    }
+}
