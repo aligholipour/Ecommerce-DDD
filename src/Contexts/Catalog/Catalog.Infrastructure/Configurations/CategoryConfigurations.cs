edaf1828f76f4b0b6194c774d211a9f030527fa9
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-03-19 13:51:53 +0330

    Add Category Configurations

---
+++ src/Contexts/Catalog/Catalog.Infrastructure/Configurations/CategoryConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Domain.Entities.Categories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catalog.Infrastructure.Configurations
+{
+    public class CategoryConfigurations : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(70);
+        }
+    }
+}
