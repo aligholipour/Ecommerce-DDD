bf3011b58b0191a05803f2c7935207b84f42b635
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-03-20 01:03:41 +0330

    Add AddCategoryCommand

---
+++ src/Contexts/Catalog/Catalog.Application/Categories/UseCases/Commands/AddCategoryCommand.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Application.Categories.Dtos;
+using MediatR;
+
+namespace Catalog.Application.Categories.UseCases.Commands
+{
+    public class AddCategoryCommand : IRequest<int>
+    {
+        public AddCategoryDto addCategoryDto { get; private set; }
+        public AddCategoryCommand(AddCategoryDto addCategoryDto)
+        {
+            this.addCategoryDto = addCategoryDto;
+        }
+    }
+}
