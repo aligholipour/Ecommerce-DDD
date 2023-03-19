26d9a6a1bd3aac9b286dd32b1bf883db144081f8
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-03-20 01:12:30 +0330

    Add AddCategoryCommandHandler

---
+++ src/Contexts/Catalog/Catalog.Application/Categories/UseCases/Handlers/AddCategoryCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Catalog.Application.Categories.UseCases.Commands;
+using Catalog.Domain.Entities.Categories;
+using MediatR;
+
+namespace Catalog.Application.Categories.UseCases.Handlers
+{
+    public class AddCategoryCommandHandler : IRequestHandler<AddCategoryCommand, int>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public AddCategoryCommandHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(AddCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = _mapper.Map<Category>(request.addCategoryDto);
+
+            await _categoryRepository.AddProductAsync(category);
+
+            return category.Id;
+        }
+    }
+}
