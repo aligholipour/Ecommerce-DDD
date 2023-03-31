5e0dd461f1472ea88eef4461a55c47b357739abc
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-01 00:25:17 +0430

    Add GetCategoryDto

---
+++ src/Contexts/Catalog/Catalog.Application/Categories/Dtos/GetCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Application.Categories.Dtos
+{
+    public class GetCategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public int ParentCategoryId { get; set; }
+        public bool Published { get; set; }
+    }
+}
