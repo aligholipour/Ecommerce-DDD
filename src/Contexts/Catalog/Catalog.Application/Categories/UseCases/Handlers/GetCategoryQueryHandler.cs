4810488a551305523e7b82f3c0cb9ea79a8fa0c0
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-01 00:41:51 +0430

    Add GetCategoryQueryHandler

---
+++ src/Contexts/Catalog/Catalog.Application/Categories/UseCases/Handlers/GetCategoryQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Catalog.Application.Categories.Dtos;
+using Catalog.Application.Categories.UseCases.Queries;
+using Catalog.Domain.Entities.Categories;
+using MediatR;
+
+namespace Catalog.Application.Categories.UseCases.Handlers
+{
+    public class GetCategoryQueryHandler : IRequestHandler<GetCategoryQuery, GetCategoryDto>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetCategoryQueryHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetCategoryDto> Handle(GetCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var category = await _categoryRepository.GetCategoryById(request.CategoryId);
+
+            var result  = _mapper.Map<GetCategoryDto>(category);
+
+            return result;
+        }
+    }
+}
