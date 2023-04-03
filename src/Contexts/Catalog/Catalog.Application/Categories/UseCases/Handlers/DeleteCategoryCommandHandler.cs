2af0aa25380c2202d7a50917ce31511394f23c40
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-03 21:22:07 +0430

    Add DeleteCategoryCommandHandler

---
+++ src/Contexts/Catalog/Catalog.Application/Categories/UseCases/Handlers/DeleteCategoryCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.Application.Categories.UseCases.Commands;
+using Catalog.Domain.Entities.Categories;
+using MediatR;
+
+namespace Catalog.Application.Categories.UseCases.Handlers
+{
+    public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public DeleteCategoryCommandHandler(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = await _categoryRepository.GetCategoryById(request.CategoryId);
+
+            await _categoryRepository.DeleteCategoryById(category);
+
+            return Unit.Value;
+        }
+    }
+}
