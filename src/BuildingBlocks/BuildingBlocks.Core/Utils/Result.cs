c0f5c31a357b06fa84e041e0555fe5d65038b53e
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-07 17:08:03 +0430

    Add Result class to return an understandable message

---
+++ src/BuildingBlocks/BuildingBlocks.Core/Utils/Result.cs
@@ -0,0 +1,27 @@
+﻿namespace BuildingBlocks.Core.Utils
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public string Error { get; }
+
+        protected Result(bool isSuccess, string error)
+        {
+            if (isSuccess && !string.IsNullOrWhiteSpace(error))
+                throw new InvalidOperationException("A successful result cannot contain an error message.");
+
+            if (!isSuccess && string.IsNullOrWhiteSpace(error))
+                throw new InvalidOperationException("A failed result must contain an error message.");
+
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Success() => new Result(true, null);
+
+        public static Result Success(string message) => new Result(true, message);
+
+        public static Result Failure(string error) => new Result(false, error); 
+    }
+}
