ad8e7925d2f75aa984d2dd4e3b21a3905a197854
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-10 01:19:24 +0430

    Use Result class in DeleteCategoryCommand

--- src/Contexts/Catalog/Catalog.Application/Categories/UseCases/Commands/DeleteCategoryCommand.cs
+++ src/Contexts/Catalog/Catalog.Application/Categories/UseCases/Commands/DeleteCategoryCommand.cs
@@ -1,8 +1,9 @@
-﻿using MediatR;
+﻿using BuildingBlocks.Core.Utils;
+using MediatR;
 
 namespace Catalog.Application.Categories.UseCases.Commands
 {
-    public class DeleteCategoryCommand : IRequest
+    public class DeleteCategoryCommand : IRequest<Result>
     {
         public int CategoryId { get; set; }
     }
