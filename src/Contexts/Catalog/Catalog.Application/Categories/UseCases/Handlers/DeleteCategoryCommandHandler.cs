e5a173c3539e94872caeb5c171bf09fa45cc5178
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-10 01:22:41 +0430

    Added Result class in DeleteCategoryCommandHandler for return specific results.

--- src/Contexts/Catalog/Catalog.Application/Categories/UseCases/Handlers/DeleteCategoryCommandHandler.cs
+++ src/Contexts/Catalog/Catalog.Application/Categories/UseCases/Handlers/DeleteCategoryCommandHandler.cs
@@ -1,10 +1,11 @@
-﻿using Catalog.Application.Categories.UseCases.Commands;
+﻿using BuildingBlocks.Core.Utils;
+using Catalog.Application.Categories.UseCases.Commands;
 using Catalog.Domain.Entities.Categories;
 using MediatR;
 
 namespace Catalog.Application.Categories.UseCases.Handlers
 {
-    public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand>
+    public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand, Result>
     {
         private readonly ICategoryRepository _categoryRepository;
 
@@ -13,13 +14,13 @@ namespace Catalog.Application.Categories.UseCases.Handlers
             _categoryRepository = categoryRepository;
         }
 
-        public async Task<Unit> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+        public async Task<Result> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
         {
             var category = await _categoryRepository.GetCategoryById(request.CategoryId);
 
             await _categoryRepository.DeleteCategoryById(category);
 
-            return Unit.Value;
+            return Result.Success();
         }
     }
 }
