d5c5aba203e09aa899d2dd66eeaddc199d261f4e
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-12 02:23:52 +0430

    Use Result class in EditProductCommand

--- src/Contexts/Catalog/Catalog.Application/Products/UseCases/Commands/EditProductCommand.cs
+++ src/Contexts/Catalog/Catalog.Application/Products/UseCases/Commands/EditProductCommand.cs
@@ -1,9 +1,10 @@
-﻿using Catalog.Application.Products.Dtos;
+﻿using BuildingBlocks.Core.Utils;
+using Catalog.Application.Products.Dtos;
 using MediatR;
 
 namespace Catalog.Application.Products.UseCases.Commands
 {
-    public class EditProductCommand : IRequest<bool>
+    public class EditProductCommand : IRequest<Result>
     {
         public EditProductDto EditProductDto { get; private set; }
 
