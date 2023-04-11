103afd6343a60f2c86251c9fd504b8c48f644585
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-11 13:59:32 +0430

    Added Result class in AddProductCommandHandler for return specific results.

--- src/Contexts/Catalog/Catalog.Application/Products/UseCases/Handlers/AddProductCommandHandler.cs
+++ src/Contexts/Catalog/Catalog.Application/Products/UseCases/Handlers/AddProductCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using BuildingBlocks.Core.Utils;
 using Catalog.Application.Products.UseCases.Commands;
 using Catalog.Domain.Entities.Products;
 using Catalog.Infrastructure.Repositories;
@@ -6,7 +7,7 @@ using MediatR;
 
 namespace Catalog.Application.Products.UseCases.Handlers
 {
-    public class AddProductCommandHandler : IRequestHandler<AddProductCommand, int>
+    public class AddProductCommandHandler : IRequestHandler<AddProductCommand, Result>
     {
         private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
@@ -17,13 +18,13 @@ namespace Catalog.Application.Products.UseCases.Handlers
             _mapper = mapper;
         }
 
-        public async Task<int> Handle(AddProductCommand request, CancellationToken cancellationToken)
+        public async Task<Result> Handle(AddProductCommand request, CancellationToken cancellationToken)
         {
             var product = _mapper.Map<Product>(request.AddProduct);
 
             await _productRepository.AddProductAsync(product);
 
-            return product.Id;
+            return Result.Success();
         }
     }
 }
