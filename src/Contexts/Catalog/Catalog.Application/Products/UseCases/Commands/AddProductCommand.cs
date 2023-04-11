3f2494d850c827c51a774e9c4e9089dadd7f652e
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-11 13:58:44 +0430

    Use Result class in AddProductCommand

--- src/Contexts/Catalog/Catalog.Application/Products/UseCases/Commands/AddProductCommand.cs
+++ src/Contexts/Catalog/Catalog.Application/Products/UseCases/Commands/AddProductCommand.cs
@@ -1,9 +1,10 @@
-﻿using Catalog.Application.Products.Dtos;
+﻿using BuildingBlocks.Core.Utils;
+using Catalog.Application.Products.Dtos;
 using MediatR;
 
 namespace Catalog.Application.Products.UseCases.Commands
 {
-    public class AddProductCommand : IRequest<int>
+    public class AddProductCommand : IRequest<Result>
     {
         public AddProductDto AddProduct { get; private set; }
 
