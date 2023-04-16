b390000dfbe601b66333cedf47a607b54989761e
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-17 01:21:06 +0430

    Use Result class in CheckoutShoppingCartCommand

--- src/Contexts/Basket/Basket.Application/ShoppingCarditems/UseCases/Commands/CheckoutShoppingCartCommand.cs
+++ src/Contexts/Basket/Basket.Application/ShoppingCarditems/UseCases/Commands/CheckoutShoppingCartCommand.cs
@@ -1,9 +1,10 @@
 ﻿using Basket.Application.ShoppingCarditems.Dtos;
+using BuildingBlocks.Core.Utils;
 using MediatR;
 
 namespace Basket.Application.ShoppingCarditems.UseCases.Commands
 {
-    public class CheckoutShoppingCartCommand : IRequest<bool>
+    public class CheckoutShoppingCartCommand : IRequest<Result>
     {
         public CheckoutShoppingCartDto CheckoutShoppingCart { get; private set; }
         public CheckoutShoppingCartCommand(CheckoutShoppingCartDto checkoutShoppingCart)
