ffceab5d781d1769307fa02a4541b129553eb630
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-21 18:08:06 +0430

    Added Order entity and aggregateroot

---
+++ src/Contexts/Order/Order.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.Domain;
+
+namespace Order.Domain.Entities.Orders
+{
+    public class Order : AggregateRoot<int>
+    {
+        private DateTime _orderDate;
+
+        public Order()
+        {
+            _orderDate = DateTime.UtcNow;
+        }
+    }
+}
