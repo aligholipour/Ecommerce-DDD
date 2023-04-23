85d23ad82084eb7fc13ba61335b33f3e7ccd0c8a
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-23 22:59:08 +0430

    Added Order Configurations and set configuration for orderDate private property

---
+++ src/Contexts/Order/Order.Infrastructure/Configurations/OrderConfigurations.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Order.Infrastructure.Configurations
+{
+    public class OrderConfigurations : IEntityTypeConfiguration<Domain.Entities.Orders.Order>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Entities.Orders.Order> builder)
+        {
+            builder.Property<DateTime>("_orderDate")
+                .UsePropertyAccessMode(PropertyAccessMode.Field)
+                .HasField("OrderDate")
+                .IsRequired();
+        }
+    }
+}
