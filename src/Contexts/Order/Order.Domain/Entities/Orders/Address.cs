ab359abf56360ad5b20525f7bf77a9d59c74b2cc
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-24 18:38:12 +0430

    Added Address as a value object

---
+++ src/Contexts/Order/Order.Domain/Entities/Orders/Address.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.Domain;
+
+namespace Order.Domain.Entities.Orders
+{
+    public class Address : ValueObject
+    {
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string Country { get; private set; }
+        public string ZipCode { get; private set; }
+
+        public Address() { }
+
+        public Address(string street, string city, string state, string country, string zipCode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            Country = country;
+            ZipCode = zipCode;
+        }
+
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Street;
+            yield return City;
+            yield return State;
+            yield return Country;
+            yield return ZipCode;
+        }
+    }
+}
