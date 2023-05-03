f704a6e51c703d1e499f7a663a2c92e49d8501b6
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-05-04 02:30:40 +0430

    Added InsertProduct method in Catalog.API project

--- src/Contexts/Catalog/Catalog.Api/Controllers/ProductController.cs
+++ src/Contexts/Catalog/Catalog.Api/Controllers/ProductController.cs
@@ -1,3 +1,6 @@
+using Catalog.Api.Models.Products;
+using Catalog.Application.Products.Dtos;
+using Catalog.Application.Products.UseCases.Commands;
 using Catalog.Application.Products.UseCases.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -26,6 +29,28 @@ namespace Catalog.Api.Controllers
             var product = await _mediator.Send(productQuery);
 
             return Ok(product);
+        }
+
+        [HttpPost("InsertProduct")]
+        public async Task<IActionResult> InsertProduct(AddProductModel productModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var productCommand = new AddProductCommand(new AddProductDto
+            {
+                Name = productModel.Name,
+                Description = productModel.Description,
+                Price = productModel.Price,
+                Quantity = productModel.Quantity
+            });
+
+            var result = await _mediator.Send(productCommand);
+
+            if (result.IsSuccess)
+                return Ok();
+
+            return BadRequest();
         }
     }
 }
