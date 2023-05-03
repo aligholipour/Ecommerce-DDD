b84f9bc714494fe8b217fd0c0d15647148ff0dfe
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-05-04 02:28:47 +0430

    Added AddProductModel for insert new product

---
+++ src/Contexts/Catalog/Catalog.Api/Models/Products/AddProductModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Api.Models.Products
+{
+    public class AddProductModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
