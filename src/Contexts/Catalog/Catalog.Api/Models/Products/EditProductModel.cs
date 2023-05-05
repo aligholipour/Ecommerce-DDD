b2a2d1320ef88218b218c5d9092365dea65aa605
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-05-05 14:50:35 +0330

    Added EditProductModel for update product

---
+++ src/Contexts/Catalog/Catalog.Api/Models/Products/EditProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.Api.Models.Products
+{
+    public class EditProductModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
