d830993d1c35db491d90451ff629cd985162562a
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-05-06 18:08:26 +0330

    Added CategoryController

---
+++ src/Contexts/Catalog/Catalog.Api/Controllers/CategoryController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+    }
+}
