c19a08ba209ce6d9fbac8ff5d8e74e69edd6e93d
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-05-06 18:12:41 +0330

    Added GetCategory method in Catalog.API project

--- src/Contexts/Catalog/Catalog.Api/Controllers/CategoryController.cs
+++ src/Contexts/Catalog/Catalog.Api/Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Catalog.Application.Categories.UseCases.Queries;
+using Catalog.Application.Products.UseCases.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.Api.Controllers
 {
@@ -6,5 +9,24 @@ namespace Catalog.Api.Controllers
     [Route("[controller]")]
     public class CategoryController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public CategoryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetCategory")]
+        public async Task<IActionResult> GetCategory(int id)
+        {
+            var categoryQuery = new GetCategoryQuery
+            {
+                CategoryId = id
+            };
+
+            var category = await _mediator.Send(categoryQuery);
+
+            return Ok(category);
+        }
     }
 }
