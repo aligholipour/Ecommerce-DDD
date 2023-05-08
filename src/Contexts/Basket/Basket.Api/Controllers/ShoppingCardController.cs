460065063e80601744d821abd7425895bcdcf124
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-05-08 17:31:51 +0330

    Remove redundant classes in Basket.Api

---
+++ src/Contexts/Basket/Basket.Api/Controllers/ShoppingCardController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShoppingCardController : ControllerBase
+    {
+
+    }
+}
