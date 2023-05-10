cc59b6368e214ec69a11c02fb6a5a0a18c5ee2b8
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-05-10 23:11:52 +0330

    Added AddShoppingCarItem method in Basket.Api project

--- src/Contexts/Basket/Basket.Api/Controllers/ShoppingCardController.cs
+++ src/Contexts/Basket/Basket.Api/Controllers/ShoppingCardController.cs
@@ -1,3 +1,7 @@
+using Basket.Api.Models.ShoppingCardItems;
+using Basket.Application.ShoppingCarditems.Dtos;
+using Basket.Application.ShoppingCarditems.UseCases.Commands;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basket.Api.Controllers
@@ -6,6 +10,32 @@ namespace Basket.Api.Controllers
     [Route("[controller]")]
     public class ShoppingCardController : ControllerBase
     {
+        private readonly IMediator _mediator;
 
+        public ShoppingCardController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("AddShoppingCarItem")]
+        public async Task<ActionResult> AddShoppingCarItem(ShoppingCartItemCreateModel shoppingCartItemModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var shoppingCartItemCommand = new CreateShoppingCartItemCommand(new ShoppingCartItemCreateDto
+            {
+                Amount = shoppingCartItemModel.Amount,
+                ProductID = shoppingCartItemModel.ProductID,
+                UserID = shoppingCartItemModel.UserID,
+            });
+
+            var result = await _mediator.Send(shoppingCartItemCommand);
+
+            if (result.IsSuccess)
+                return Ok();
+
+            return BadRequest();
+        }
     }
 }
