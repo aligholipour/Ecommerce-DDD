e93b5dd38024d844480371f731f56f8cf396d101
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-05-10 22:52:49 +0330

    Added ShoppingCartItemCreateModel for add shopping card item

---
+++ src/Contexts/Basket/Basket.Api/Models/ShoppingCardItems/ShoppingCartItemCreateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.Api.Models.ShoppingCardItems
+{
+    public class ShoppingCartItemCreateModel
+    {
+        public string UserID { get; set; }
+        public string ProductID { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
