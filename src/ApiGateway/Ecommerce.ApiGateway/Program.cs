5ae93b7eb2e470e5f533557c0c2e11682dfa91f0
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-06-02 16:43:51 +0330

    Added Ocelot configurations in program.cs

--- src/ApiGateway/Ecommerce.ApiGateway/Program.cs
+++ src/ApiGateway/Ecommerce.ApiGateway/Program.cs
@@ -1,8 +1,21 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath);
+builder.Configuration.AddJsonFile("appsettings.json", false, true);
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", false, true);
+builder.Configuration.AddJsonFile("ocelot.json", false, true);
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddOcelot(builder.Configuration);
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseOcelot().Wait();
 
 app.UseHttpsRedirection();
 
