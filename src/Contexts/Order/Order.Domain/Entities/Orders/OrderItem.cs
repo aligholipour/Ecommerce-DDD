03fb9226b74939807667e2a5d22766459818972c
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-07-14 10:27:49 +0330

    Added OrderItem entity

---
+++ src/Contexts/Order/Order.Domain/Entities/Orders/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿using BuildingBlocks.Domain;
+
+namespace Order.Domain.Entities.Orders
+{
+    public class OrderItem : Entity<int>
+    {
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public int OrderId { get; set; }
+    }
+}
