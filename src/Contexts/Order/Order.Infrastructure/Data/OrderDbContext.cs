1518f1334375c354a50f662d513213004b0488c2
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-07-27 19:47:36 +0330

    Added OrderDbContext in order bounded context

---
+++ src/Contexts/Order/Order.Infrastructure/Data/OrderDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Order.Infrastructure.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> option) : base(option)
+        {
+        }
+    }
+}
